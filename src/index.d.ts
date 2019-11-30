93fa79c857781548e3a10dad459a0d3c2eb62fb8
Carson Farmer <carson.farmer@gmail.com>
2019-11-29 21:52:16 -0800

    feat: typescript types
    Offering to submit and maintain these types. Given the relative stability of js-cids, this seems like a low maintenance PR.

---
+++ src/index.d.ts
@@ -0,0 +1,29 @@
+export type Version = 0 | 1
+export type Codec = string
+export type Multihash = Buffer
+export type BaseEncodedString = string
+export type MultibaseName = string
+
+export class CID {
+  constructor(version: Version, codec: Codec, multhash: Multihash, multibaseName?: MultibaseName)
+  constructor(cidStr: BaseEncodedString)
+  constructor(cidBuf: Buffer)
+  constructor(cidMultihash: Multihash)
+  constructor(cid: CID)
+  codec: Codec
+  multihash: Multihash
+  buffer: Buffer
+  prefix: Buffer
+  version: number
+  toV0(): CID
+  toV1(): CID
+  toBaseEncodedString(base?: string): BaseEncodedString
+  toString(): BaseEncodedString
+  toJSON(): { codec: Codec, version: Version, hash: Multihash }
+  equals(other: any): boolean
+  static codecs: Record<string, Codec>
+  static isCID(mixed: any): boolean
+  static validateCID(other: any): void
+}
+
+export default CID
