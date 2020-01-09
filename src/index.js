4a78381f64e326cac64af3e9c80bdcdaa00c95e9
Ilir  Hushi <ilirhushi@gmail.com>
2020-01-09 15:50:02 +0100

    Angular 8 fix braking change
    Angular 8 fix braking change of not resolving 'multicodec/src/base-table'

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
 const mh = require('multihashes')
 const multibase = require('multibase')
 const multicodec = require('multicodec')
-const codecs = require('multicodec/src/base-table')
+const codecs = require('multicodec/src/init-table')
 const CIDUtil = require('./cid-util')
 const withIs = require('class-is')
 
