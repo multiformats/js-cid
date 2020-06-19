e6ebb7ad870655dec660899b5a6016949cf3d748
achingbrain <alex@achingbrain.net>
2020-06-19 18:21:46 +0100

    chore: remove multihashing dependency

--- test/profiling/cidperf-x.js
+++ test/profiling/cidperf-x.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-console */
 'use strict'
 
-const multihashing = require('multihashing')
+const multihashing = require('multihashing-async')
 // [1] Original/existing implementation.
 // const CID = require('cids')
 // [2] New/proposed implementation.
@@ -16,10 +16,10 @@ function sleep (ms) {
 // The purpose of this class is
 // to simply test the CID ctor (and, primarily, CID.isCID() method).
 class CIDPerfX {
-  constructor () {
+  constructor (mh) {
     this.version = 1
     this.codec = 'dag-pb'
-    this.mh = multihashing(Buffer.from('oh, hey!'), 'sha2-256')
+    this.mh = mh
   }
 
   // i: Running-counter.
@@ -39,14 +39,18 @@ class CIDPerfX {
 // /////////////////////////
 
 const reps = 10000
-const cidPerf = new CIDPerfX()
 
 console.log(`Test: Will run "new CID()" ${reps} times.`)
 // We just give ~1 second for the JS engine to start and 'rest', etc.
 // before starting new tests.
-sleep(1000).then(() => {
+sleep(1000).then(async () => {
   console.log('Starting a test...')
-  console.time('run'); [...Array(reps).keys()].map(i => {
+  console.time('run')
+
+  const mh = await multihashing(Buffer.from('oh, hey!'), 'sha2-256')
+  const cidPerf = new CIDPerfX(mh);
+
+  [...Array(reps).keys()].map(i => {
     cidPerf.run(i)
   })
   console.timeEnd('run')
