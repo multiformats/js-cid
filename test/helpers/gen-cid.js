0fa23c7fa41407c806da70939ae1966be6de3fa7
Alex Potsides <alex@achingbrain.net>
2020-06-19 18:33:27 +0100

    chore: update deps and add Buffer require (#114)
    remove multihashing dependency

--- test/helpers/gen-cid.js
+++ test/helpers/gen-cid.js
@@ -5,19 +5,28 @@
 
 const multibase = require('multibase')
 const codecs = require('../../src').codecs
-const multihashing = require('multihashing')
+const multihashing = require('multihashing-async')
+const { Buffer } = require('buffer')
 
-const mh = multihashing(Buffer.from('oh, hey!'), 'sha2-256')
+async function main () {
+  const mh = await multihashing(Buffer.from('oh, hey!'), 'sha2-256')
 
-const cid = Buffer.concat([
-  Buffer.from('01', 'hex'),
-  codecs.dagPB,
-  mh
-])
+  const cid = Buffer.concat([
+    Buffer.from('01', 'hex'),
+    Buffer.from([codecs['dag-pb']]),
+    mh
+  ])
 
-const cidStr = multibase.encode('base58btc', cid).toString()
+  const cidStr = multibase.encode('base58btc', cid).toString()
 
-console.log('CID String (multibase included)')
-console.log(cidStr)
-console.log('CID in hex (multibase not included)')
-console.log(cid.toString('hex'))
+  console.log('CID String (multibase included)')
+  console.log(cidStr)
+  console.log('CID in hex (multibase not included)')
+  console.log(cid.toString('hex'))
+}
+
+main()
+  .catch(err => {
+    console.error(err)
+    process.exit(0)
+  })
